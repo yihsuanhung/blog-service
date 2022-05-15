badd58702e3dae5f10ef778918cc240deba4f3ef
Simon Hung <simon@MacBook-Pro.local>
2022-05-15 16:18:52 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	routers "github.com/yihsuanhung/blog-service/internal/routes"
+)
+
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
